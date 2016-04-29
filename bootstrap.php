212941986c68b1284b4ef9358778c40f09467ecb
Fernando <fernando.espinosa@ge.com>
2016-04-28 22:37:27 -0500

    New classes registered on bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,16 +4,18 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Include Mapache and GEIQ API
-include 'C:/xampp/htdocs/cf-ui/framework/mapache/1/Mapache.php';
+include 'C:/xampp/htdocs/cf_ui/cf-ui/framework/mapache/1/Mapache.php';
 //C:\xampp\htdocs\cf-ui\framework\mapache\1
 
 // Prepare Mapache configuration
 $config = Array(
     'mapache' => array(
-        'root_path' => 'C:/xampp/htdocs/cf-ui',
+        'root_path' => 'C:/xampp/htdocs/cf_ui/cf-ui',
         'include' => Array(
             'cf\CloudFoundry' => 'components/cf/CloudFoundry.php',
-            'cf\Apps' => 'components/cf/Apps.php'
+            'cf\Apps' => 'components/cf/Apps.php',
+            'cf\Spaces' => 'components/cf/Spaces.php',
+            'cf\Services' => 'components/cf/Services.php'
         )
     ),
     'proxy' => 'iss-americas-pitc-cincinnatiz.proxy.corporate.ge.com:80'
